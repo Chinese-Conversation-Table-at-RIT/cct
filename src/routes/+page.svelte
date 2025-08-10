2ff124ae09a152bc5d2e9dd1ceacdfa845a7675c
Xuejian Sundvall <jiansundvall@gmail.com>
2025-08-10 14:01:55 -0500

    add placeholder text to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,26 +6,17 @@
 
 <svelte:head>
 	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<meta name="description" content="Chinese Conversation Table club" />
 </svelte:head>
 
 <section>
 	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcomeFallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
+        Welcome to the Chinese Conversation Table
 	</h1>
 
 	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
+		We are a really good club.
 	</h2>
-
-	<Counter />
 </section>
 
 <style>
@@ -39,21 +30,5 @@
 
 	h1 {
 		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
 	}
 </style>
