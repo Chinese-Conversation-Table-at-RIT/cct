42f8f932655e5b68b83fc46dddc0fb98fb535ef2
Xuejian Sundvall <jiansundvall@gmail.com>
2025-08-10 14:03:39 -0500

    add placeholder text for home apge

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,3 @@
-<script lang="ts">
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcomeFallback from '$lib/images/svelte-welcome.png';
-</script>
-
 <svelte:head>
 	<title>Home</title>
 	<meta name="description" content="Chinese Conversation Table club" />
