4188fc7d0501d9cb75d896194466329e39dc46c9
Xuejian Sundvall <jiansundvall@gmail.com>
2025-08-16 13:30:56 -0500

    add real google calendar link

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -5,5 +5,5 @@
 
 <div class="text-column">
 	<h1>Events</h1>
-    <iframe src="https://calendar.google.com/calendar/embed?src=xs3171%40g.rit.edu&ctz=America%2FNew_York" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+    <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FNew_York&showPrint=0&src=Y19mODZhNDI4MzY5ODE0NGI0Zjg3NjBhOGI5ODk2MTdjZmNiN2Q0Yjc2MmVhNmNjNzJmOWNmZmMwYjIxNDQ1YzA4QGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23f4511e" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
 </div>
